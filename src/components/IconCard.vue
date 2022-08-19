b2e80c1c61002ffef18715f1328d14b372b5dd7b
Eduardo de Cesaro <cesaroeduardo@icloud.com>
2022-08-19 18:32:52 -0300

    Added the search bar function

--- src/components/IconCard.vue
+++ src/components/IconCard.vue
@@ -1,18 +1,19 @@
 <template>
     <li class="library-module">
         <div class="library-module-container">
-            <p class="name" id="name">{{ name }}</p>
+            <p class="name" id="name">{{ name }} {{ svgCode }}</p>
             <span class="keywords">{{ keywords }}</span>
             <a title="add" class="library-module-flex-container" id="svg">
-                <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg" style="fill: rgb(0, 0, 0); width: 40px; height: 40px;"><path d="{{ teste }}"></path></svg>
+                <unicon name="plus"/>
+                {{ icon }}
             </a>
             <div class="action-list">
-                <button title="Download SVG" onclick="downloadSVG()">
+                <button title="Download SVG" @click="downloadSVG">
                     <svg focusable="false " preserveAspectRatio="xMidYMid meet " fill="currentColor " aria-label="Download SVG " aria-hidden="true " width="16 " height="16 " viewBox="0 0 16 16 " role="img" class="bx--btn__icon ">
                         <path d="M13 7L12.3 6.3 8.5 10.1 8.5 1 7.5 1 7.5 10.1 3.7 6.3 3 7 8 12zM13 12v2H3v-2H2v2l0 0c0 .6.4 1 1 1h10c.6 0 1-.4 1-1l0 0v-2H13z "></path>
                     </svg>
                 </button>
-                <button type="button " title="Copy code" onclick="copyCode()">
+                <button type="button " title="Copy code" @click="copyCode">
                     <svg focusable="false " preserveAspectRatio="xMidYMid meet " fill="currentColor " aria-label="Copy Code" aria-hidden="true" width="16" height="16" viewBox="0 0 32 32" role="img" class="bx--btn__icon">
                 <path d="M31 16L24 23 22.59 21.59 28.17 16 22.59 10.41 24 9 31 16zM1 16L8 9 9.41 10.41 3.83 16 9.41 21.59 8 23 1 16z"></path>
                 <path d="M5.91 15H26.080000000000002V17H5.91z" transform="rotate(-75 15.996 16)"></path>
@@ -29,9 +30,27 @@ export default {
   props: {
     name: String,
     keywords: String,
-    teste: String
+    icon: String
   }
 }
+
+// // CopyCode Function
+// function copyCode() {
+//     const name = document.getElementsByClassName('name')[0];
+//     navigator.clipboard.writeText("<azn-icon name='" + name.innerText.toLowerCase() + "' />");
+// }
+
+// // Download SVG Function
+// function downloadSVG() {
+//     const svg = document.getElementById('svg').innerHTML;
+//     const blob = new Blob([svg.toString()]);
+//     const element = document.createElement("a");
+//     const name = document.getElementsByClassName('name')[0];
+//     element.download = name.innerText.toLowerCase() + ".svg";
+//     element.href = window.URL.createObjectURL(blob);
+//     element.click();
+//     element.remove();
+// }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,35 @@
+<template>
+   <input type="text" v-on:keyup="searchIcons()" id="searchInput" v-model="input" placeholder="Search icons..." />
+</template>
+
+<script setup>
+import { ref } from "vue";
+let input = ref("");
+function searchIcons() {
+    // Declare variables
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('searchInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('li');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("span")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+</script>
+
+<style scoped>
+#searchInput {
+    height: 48px;
+    min-width: 24rem;
+    padding-left: 1rem;
+}
+</style>
