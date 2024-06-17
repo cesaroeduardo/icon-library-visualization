560cd2beb4f237922f325f09c469aeabb10dfe17
Eduardo de Cesaro <44036260+cesaroeduardo@users.noreply.github.com>
2024-06-17 03:15:01 -0300

    solve errors

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import { createApp } from 'vue';
-import './assets/main.css';
-import App from './App.vue';
-import Vue3ColorPicker from "vue3-colorpicker";
-import './assets/azionicons.scss';
-import "vue3-colorpicker/style.css";
-import { initializeTheme } from './theme';
+import { createApp } from 'vue'
+import './assets/main.css'
+import App from './App.vue'
+import Vue3ColorPicker from "vue3-colorpicker"
+import './assets/azionicons.scss'
+import "vue3-colorpicker/style.css"
+import { initializeTheme } from './theme'
 
 initializeTheme();
 
