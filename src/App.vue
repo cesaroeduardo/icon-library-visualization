17baaece07bca6e63c98727a47ddacb2d997bfbd
Eduardo de Cesaro <44036260+cesaroeduardo@users.noreply.github.com>
2024-06-21 00:39:41 -0300

    test deploy

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col justify-center items-center w-full">
     <!-- Top Bar -->
     <div class="w-full bg-neutral-200 dark:bg-neutral-900 overflow-hidden pb-5 gradient-mask-b-60 md:gradient-mask-b-60 px-4 md:px-8 top-0 z-20 py-4 md:py-8 sticky justify-center items-center flex flex-col">
-      <div class="max-w-[1480px] w-full z-20 flex gap-2 md:gap-4 justify-between">
+      <div class="max-w-[1472px] w-full z-20 flex gap-2 md:gap-4 justify-between">
         <SearchBar class="w-full z-20" />
         <button @click="toggleTheme" class="z-20">
           <i :class="getThemeIcon()"></i>
@@ -104,8 +104,8 @@ export default {
     return {
       isDarkMode,
       getThemeIcon,
-      IconsColor: 'rgb(140, 98, 213)',
-      circleColors: ['rgb(140, 98, 213)', 'rgb(140, 98, 213)', 'rgb(140, 98, 213)'],
+      IconsColor: 'rgb(133, 133, 133)',
+      circleColors: ['rgb(133, 133, 133)', 'rgb(133, 133, 133)', 'rgb(133, 133, 133)'],
       circlePositions: [
         { top: '-30%', left: '-30%' },
         { top: '20%', left: '90%' },
@@ -125,7 +125,7 @@ export default {
         { name: '56px', value: 'text-5xl' },
         { name: '64px', value: 'text-6xl' },
       ],
-      pureColor: 'rgb(140, 98, 213)',
+      pureColor: 'rgb(133, 133, 133)',
       downloadFormat: 'svg'
     }
   },
